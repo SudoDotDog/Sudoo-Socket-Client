5f2f6f31ec07b4974b513bc5fd9881f916c1268d
WMXPY <wm@mengw.io>
2021-11-06 17:27:59 -0500

    export authorization

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,6 @@ export * from "./client/connection";
 export * from "./client/declare";
 export * from "./client/message-handler";
 export * from "./client/node";
+export * from "./util/authorization";
 export * from "./util/url";
 
